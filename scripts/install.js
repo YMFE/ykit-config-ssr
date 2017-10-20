c07bd1e4449546b18e69b686fb48e89ddcb9da4d
yuhao.ju <yuhao.ju@qunar.com>
2017-10-20 19:42:57 +0800

    fix: 修复已经有的 ssr 会被覆盖

--- scripts/install.js
+++ scripts/install.js
@@ -2,9 +2,12 @@
 var fs = require('fs-extra');
 var path = require('path');
 
-const appRoot = __dirname.split('node_modules')[0];
+var appRoot = __dirname.split('node_modules')[0];
+var ykitJSContent = fs.readFileSync(path.join(__dirname, '../template/ykit.js'), 'utf-8');
 
-fs.copySync(
-    path.join(__dirname, '../template/ykit.js'),
-    path.join(appRoot, 'ykit.js')
-);
+if(ykitJSContent.indexOf('ssr') === -1) {
+    fs.copySync(
+        path.join(__dirname, '../template/ykit.js'),
+        path.join(appRoot, 'ykit.js')
+    );
+}
